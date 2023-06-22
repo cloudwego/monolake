a73b307b868beff9ce576e4bbe4d817f3cbd2dca
Rain Jiang <rain-jiang@outlook.com>
2023-06-22 00:20:08 +0000

    remove the HttpAccept since it is not required

--- monolake-core/src/http/mod.rs
+++ monolake-core/src/http/mod.rs
@@ -1,9 +1,7 @@
 mod rewrite;
-use std::{future::Future, io::Cursor};
+use std::future::Future;
 
-use bytes::BytesMut;
 use http::{Request, Response};
-use monoio::io::PrefixedReadIo;
 use monoio_http::h1::payload::Payload;
 pub use rewrite::Rewrite;
 use service_async::Service;
@@ -15,9 +13,6 @@ pub type HttpError = anyhow::Error;
 // Response and a bool indicating should continue processing the connection.
 // Service does not need to add `Connection: close` itself.
 pub type ResponseWithContinue = (Response<Payload>, bool);
-
-pub type HttpAccept<Stream, SocketAddr> =
-    (bool, PrefixedReadIo<Stream, Cursor<BytesMut>>, SocketAddr);
 
 pub trait HttpHandler: Sealed {
     type Error;
